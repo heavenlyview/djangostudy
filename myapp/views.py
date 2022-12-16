958a8955de4ade2637356ac3f4d54a6e4e8cf8e7
heavenlyview <heavenlyview12@gmail.com>
2022-12-16 15:50:50 +0900

    django 7 homepage read function

--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render, HttpResponse
 import random # 접속이 들어올 때마다 랜덤한 정보를 동적으로 만들어주는 웹 애플리케이션을 만들어 줄 때에 필요함
-# topics = [
-#     {'id':1, 'title':'routing', 'body':'Routing is ..'},
-#     {'id':2, 'title':'view', 'body':'View is ..'},
-#     {'id':3, 'title':'Model', 'body':'Model is ..'},
-# ]
+topics = [
+    {'id':1, 'title':'routing', 'body':'Routing is ..'},
+    {'id':2, 'title':'view', 'body':'View is ..'},
+    {'id':3, 'title':'Model', 'body':'Model is ..'},
+]
 
 def index(request):
-    # global topics # topics는 전역변수다라고 선언
-    # ol = ''
-    # for topic in topics:
-    #     ol += f'<li>{topic["title"]}</li>'
-
-    return HttpResponse('''
+    global topics # topics는 전역변수다라고 선언
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+        
+    return HttpResponse(f'''
     <html>
     <body>
         <h1>Django</h1>
         <ol>
-            <li>routing</li>
-            <li>view</li>
-            <li>model</li>
+            {ol}
         <ol>
         <h2>welcome</h2>
         Hello, Django
