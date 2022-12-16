7bfeb14fe7cabb87036a0d2895dfe468be78303a
heavenlyview <heavenlyview12@gmail.com>
2022-12-16 14:43:56 +0900

    django 7 - read

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render, HttpResponse
 import random # 접속이 들어올 때마다 랜덤한 정보를 동적으로 만들어주는 웹 애플리케이션을 만들어 줄 때에 필요함
+# topics = [
+#     {'id':1, 'title':'routing', 'body':'Routing is ..'},
+#     {'id':2, 'title':'view', 'body':'View is ..'},
+#     {'id':3, 'title':'Model', 'body':'Model is ..'},
+# ]
 
-
-# Create your views here.
 def index(request):
-    return HttpResponse('<h1>Random</h1>'+str(random.random())) # 실행할 때마다 랜덤 숫자 출력
-# str로 바꿔주는 이유는 '+' 기준, 왼쪽으로는 문자형인데 오른쪽은 숫자형이라 문자형으로 바꿔주기 위함
+    # global topics # topics는 전역변수다라고 선언
+    # ol = ''
+    # for topic in topics:
+    #     ol += f'<li>{topic["title"]}</li>'
+
+    return HttpResponse('''
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            <li>routing</li>
+            <li>view</li>
+            <li>model</li>
+        <ol>
+        <h2>welcome</h2>
+        Hello, Django
+    </body>
+    </html>
+    ''') 
+
 def create(request):
     return HttpResponse('Create')
 
