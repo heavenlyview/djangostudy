17373bcb45942add48e528564385ed168c121715
heavenlyview <heavenlyview12@gmail.com>
2022-12-20 14:34:31 +0900

    Django Web Framework - 13 modify

--- myapp/views.py
+++ myapp/views.py
@@ -21,6 +21,7 @@ def HTMLTemplate(articleTag, id=None):
                     <input type="submit" value="delete">
                 </form>
             </li>
+            <li>a href="/update/{id}"</li>
         '''
     ol = ''
     for topic in topics:
@@ -73,6 +74,34 @@ def create(request):
         url = '/read/'+ str(nextId)
         nextId = nextId + 1
         return redirect(url)
+
+@csrf_exempt
+def update(request, id):
+    global topics
+    if request.method == 'GET':
+        for topic in topics:
+            if topic['id'] == int(id):
+                selectedTopic = {
+                    "title":topic['title'],
+                    "body":topic['body']
+                }
+        article = '''
+            <form action="/update/"{id}/" method="post">
+                <p><input type="text" name="title" placeholder="title" value={selectedTopic["title"]}></p>
+                <p><textarea name="body" placeholder="body">{selectedTopic['body']}</textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article, id))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic['title'] = title
+                topic['body'] = body
+        return redirect(f'/read/{id}')
+
 
 @csrf_exempt
 def delete(request):
